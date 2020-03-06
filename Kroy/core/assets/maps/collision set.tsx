ba591bd4f31b41ce47d53e44ea333cc904e88f80
LeahMoss <lam589@york.ac.uk>
2020-03-06 11:52:44 +0000

    Updated and fixed map again

---
+++ Kroy/core/assets/maps/collision set.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="collision set" tilewidth="48" tileheight="48" tilecount="100" columns="10">
+ <image source="../../../../../Assessment3/kroy-assessment3/SEPRKroy/core/assets/maps/collision_objects.png" width="480" height="480"/>
+ <tile id="0">
+  <properties>
+   <property name="road" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="road" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ Kroy/core/assets/maps/color city cops tileset.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="color city cops tileset" tilewidth="48" tileheight="48" tilecount="168" columns="8">
+ <image source="../../../../../Assessment3/kroy-assessment3/SEPRKroy/core/assets/maps/ccc_tileset.png" width="385" height="1008"/>
+ <tile id="62">
+  <animation>
+   <frame tileid="62" duration="300"/>
+   <frame tileid="54" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="70">
+  <animation>
+   <frame tileid="70" duration="100"/>
+   <frame tileid="71" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="102">
+  <animation>
+   <frame tileid="102" duration="300"/>
+   <frame tileid="94" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="127">
+  <animation>
+   <frame tileid="127" duration="300"/>
+   <frame tileid="135" duration="350"/>
+  </animation>
+ </tile>
+ <tile id="142">
+  <animation>
+   <frame tileid="142" duration="100"/>
+   <frame tileid="143" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
