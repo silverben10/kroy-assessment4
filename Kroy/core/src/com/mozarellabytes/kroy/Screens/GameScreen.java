aa3b7bb48866f03214dc90e81c314f49da87ac37
Ben Silverman <bensilverman10@gmail.com>
2020-04-23 16:18:34 +0100

    Fix game GUI not responding after closing controls screen

--- Kroy/core/src/com/mozarellabytes/kroy/Screens/GameScreen.java
+++ Kroy/core/src/com/mozarellabytes/kroy/Screens/GameScreen.java
@@ -178,6 +178,7 @@ public class GameScreen implements Screen {
 
     @Override
     public void show() {
+        Gdx.input.setInputProcessor(new GameInputHandler(this, gui));
     }
 
     @Override
